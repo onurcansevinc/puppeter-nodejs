ff09d768daa59efec72c5dc07662677bcd42d7ba
Onurcan Sevinc <onurcan.svnc@yandex.com>
2025-05-18 23:21:52 +0300

    fix: Update entrypoint script to set internal Docker IP and modify startup command execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+cd /home/container
 
-# Start the application
-exec "$@"
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
-@echo "Hello World"
+# Print Node.js Version
+node -v
 
-node --version
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-npm --version
-
-
+# Run the Server
+eval ${MODIFIED_STARTUP}
